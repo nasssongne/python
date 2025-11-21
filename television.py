736793439f2414ce6c174f9c8361f7fe53de301f
nasssongne <155864798+nasssongne@users.noreply.github.com>
2025-11-20 19:23:41 -0600

    Update television.py

--- television.py
+++ television.py
@@ -6,10 +6,10 @@ class Television:
 
     def __init__(self) -> None:
         """Initialize the Television"""
-        self.__status: bool = False
-        self.__muted: bool = False
-        self.__volume: int = Television.MIN_VOLUME
-        self.__channel: int = Television.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self) -> None:
         """Toggle the TV power on/off."""
@@ -37,7 +37,7 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self) -> None:
-        """Increase volume"""
+        """Increase the volume"""
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -45,7 +45,7 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self) -> None:
-        """Decrease volume"""
+        """Decrease the volume"""
         if self.__status:
             if self.__muted:
                 self.__muted = False
